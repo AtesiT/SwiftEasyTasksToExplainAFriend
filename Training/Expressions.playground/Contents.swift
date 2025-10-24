1b5e8bb934696f772e78169697b37c404797317d
AtesiT <atesit@mail.ru>
2025-10-24 23:19:01 +0300

    add \\s to check space in the sentence

--- Training/Expressions.playground/Contents.swift
+++ Training/Expressions.playground/Contents.swift
@@ -7,10 +7,6 @@ import Foundation
 // \w = underscopes, letters, digits
 // \d = any digit
 
-// \S \W \D - opposite actions
-
-
-
 let word = "any word"
 if let range = word.range(of: "\\w+", options: .regularExpression) {
     print("Found word: \(word[range])")
@@ -24,5 +20,15 @@ if let rangeDigits = digits.range(of: "\\d+", options: .regularExpression) {
 } else {
     print("No digits found")
 }
+
+let spaces = "Hello. What is your name?"
+if let rangeSpaces = spaces.range(of: "\\s+", options: .regularExpression) {
+    print("The sentence has space")
+} else {
+    print("The sentence doesn't have spaces")
+}
+
+
+// \S \W \D - opposite actions
 
 
