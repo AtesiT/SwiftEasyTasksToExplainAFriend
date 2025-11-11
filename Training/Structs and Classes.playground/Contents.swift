1de575090ae30d8473f3e16846a77d0e3e776e75
AtesiT <atesit@mail.ru>
2025-11-11 13:23:23 +0300

    practice: was used classes

--- Training/Structs and Classes.playground/Contents.swift
+++ Training/Structs and Classes.playground/Contents.swift
@@ -6,10 +6,7 @@ struct Building {
     func recognizePrice() -> Int {
         return price
     }
-    
-    init(price: Int) {
-        self.price = price
-    }
+    // FIX. IT WAS A STRUCT NOT A CLASS
 }
 
 let buildingOne = Building(price: 5000)
@@ -17,4 +14,39 @@ print(buildingOne.recognizePrice())
 print(buildingOne.price)
 
 
+class Vehicle {
+    var price: Int
+    func recognizePrice() -> Int {
+        return price
+    }
+    
+    init(price: Int) {
+        self.price = price
+    }
+}
+// Classes need init
 
+
+let vehicleOne = Vehicle(price: 10000)
+print(vehicleOne)
+// Needs to use protocol to describe
+
+
+
+class Aiplane: Vehicle {
+    let maxHeightFlight: Int
+    
+    func describeTheAirplane() -> String {
+        "This airplane can fly up to \(maxHeightFlight) meters. Price: \(price)"
+    }
+    
+    init(maxHeightFlight: Int, price: Int) {
+        self.maxHeightFlight = maxHeightFlight
+        super.init(price: price)
+    }
+}
+
+let airplaneOne = Aiplane(maxHeightFlight: 50, price: 75000)
+print(airplaneOne.price)
+print(airplaneOne.maxHeightFlight)
+print(airplaneOne.describeTheAirplane())
