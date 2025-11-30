0622063b40b37f07da2c2b33c295126a4302c97c
AtesiT <atesit@mail.ru>
2025-11-30 12:57:29 +0300

    was added: practice with switch case and enum in enum (for type of Macbook)

--- Training/enumPractice.playground/Contents.swift
+++ Training/enumPractice.playground/Contents.swift
@@ -1,19 +1,47 @@
 enum Laptops {
-    case Macbook
+    case Macbook(type: Version)
     case Dell
     case Microsoft
     
+    enum Version {
+        case Air
+        case Normal
+        case Pro
+    }
+    
     var title: String {
         switch self {
-        case Laptops.Macbook:
+        case Laptops.Macbook:   //  Can just a .Macbook
             return "Your laptop is Macbook"
-        case Laptops.Dell:
+        case Laptops.Dell:      //  Can just a .Dell
             return "Your laptop is Dell"
-        case Laptops.Microsoft:
+        case Laptops.Microsoft: //  Can just a .Microsoft
             return "Your laptop is Microsoft"
         }
     }
 }
 
-print(Laptops.Macbook)
-print(Laptops.Macbook.title)
+let laptop: Laptops = Laptops.Dell // Can just a .Dell
+print(laptop)
+print(laptop.title)
+
+/*
+                After update case Macbook (was added type: Price)
+*/
+
+let newLaptop: Laptops = .Macbook(type: Laptops.Version.Air)  //  Can just a .Air
+
+switch newLaptop {
+case Laptops.Macbook(let macVersion) where macVersion == .Air:
+    print("Your Macbook has \(Laptops.Version.Air) version. Price of 999$")
+case Laptops.Macbook(type: .Pro):
+    print("Your Maxbook has \(Laptops.Version.Pro) version. Price of 1299$")
+case Laptops.Dell:
+    print(laptop.title)
+case Laptops.Microsoft:
+    print(laptop.title)
+default:
+    print("You have no laptop")
+}
+
+
