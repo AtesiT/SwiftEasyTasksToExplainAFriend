6dad229d4397169c0f921d5065895280481055fb
AtesiT <atesit@mail.ru>
2025-11-30 14:08:58 +0300

    added inherit from String in enum and ALSO was practice (1 time)

--- Training/enumPractice.playground/Contents.swift
+++ Training/enumPractice.playground/Contents.swift
@@ -3,10 +3,11 @@ enum Laptops {
     case Dell
     case Microsoft
     
-    enum Version {
-        case Air
-        case Normal
-        case Pro
+    enum Version: String {
+        //  We set inherit from class "String"
+        case Air = "Air M4"
+        case Normal = "A18 Pro"
+        case Pro = "Pro M5"
     }
     
     var title: String {
@@ -33,7 +34,7 @@ let newLaptop: Laptops = .Macbook(type: Laptops.Version.Air)  //  Can just a .Ai
 
 switch newLaptop {
 case Laptops.Macbook(let macVersion) where macVersion == .Air:
-    print("Your Macbook has \(Laptops.Version.Air) version. Price of 999$")
+    print("Your Macbook has \(Laptops.Version.Air.rawValue) version. Price of 999$") // RawValue was added to displays the String
 case Laptops.Macbook(type: .Pro):
     print("Your Maxbook has \(Laptops.Version.Pro) version. Price of 1299$")
 case Laptops.Dell:
@@ -45,3 +46,58 @@ default:
 }
 
 
+/*
+            Just a test with enum rawValue
+*/
+
+enum Phone: String {
+    case iPhone = "17 Pro", Samsung = "S25", Pixel = "10"
+}
+
+let myPhone = Phone(rawValue: "17 Pro")
+print(myPhone?.rawValue ?? "Not found")
+//  Optional 'cause we can have mistake with name/existing
+
+
+
+
+/*
+                                        Practice
+                                          Self
+                                           To
+                                         Repeat
+                                        The topic
+*/
+
+
+enum Cars {
+    case Sportcar(type: Cars.SportcarModel)
+    case Hatchback(price: Cars.HatchbackPrice)
+    case SUV
+    
+    enum SportcarModel: String {
+        case Lamborghini = "Lamborghini Huracan"
+        case Ferrari = "Ferrari 2025"
+        case Bugatti = "Bugatti Chiron"
+    }
+    
+    enum HatchbackPrice {
+        case Million, Billion
+    }
+}
+
+let newCarFromHangar = Cars.SUV
+print(newCarFromHangar)
+
+let newCarFromOnlineStore = Cars.Hatchback(price: .Billion)
+print(newCarFromOnlineStore)
+
+
+enum TV: Int {
+    //  Numbers will be inch of TV
+    case LG     = 30
+    case Sony   = 44
+}
+
+let newTV = TV(rawValue: 30)
+print(newTV ?? "No TV")
