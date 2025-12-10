fd86373bacbad8c4a7af31187f1a1e32e3b6ffcb
AtesiT <atesit@mail.ru>
2025-12-10 15:51:15 +0300

    practice with numberOfSections and cellForRowAt, also practice with tableVC and cellVC

--- Training/URLSession.playground/Contents.swift
+++ Training/URLSession.playground/Contents.swift
@@ -62,3 +62,52 @@ private func fetchDatas() {
 
 fetchData()
 fetchDatas()
+
+
+//  MARK: - Practice with tableView
+import UIKit
+
+final class cellTable: UITableViewCell {
+    
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var email: UILabel!
+    
+    func configure(with anyHuman: arrayFromJSON) {
+        id.text = String(anyHuman.id)
+        name.text = anyHuman.name
+        email.text = anyHuman.email
+    }
+}
+
+
+
+
+struct arrayFromJSON {
+    let id: Int
+    let name: String
+    let email: String
+}
+
+let somebody = arrayFromJSON(id: 1, name: "John", email: "mail@email.com")
+let somebody2 = arrayFromJSON(id: 2, name: "Jane", email: "mail2@email.com")
+
+let arrayOfPeople: [arrayFromJSON] = [somebody, somebody2]
+
+
+
+
+
+final class tableViewController: UITableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        arrayOfPeople.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = cell as? cellTable else {return UITableViewCell()}
+        let human = arrayOfPeople[indexPath.row]
+        cell.configure(with: human)
+        return cell
+    }
+}
