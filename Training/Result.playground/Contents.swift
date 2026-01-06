bef29f9507fc2978d71f5d8eba6842edc878f9be
AtesiT <atesit@mail.ru>
2026-01-06 12:58:36 +0300

    MADE: was called the function to parse Data

--- Training/Result.playground/Contents.swift
+++ Training/Result.playground/Contents.swift
@@ -4,25 +4,36 @@ enum NetworkError: Error {
     case badURL
 }
 
-struct dataToDecode: Decodable {
-    let userId: String
+struct DataToDecode: Decodable {
+    let userId: Int
     let id: Int
     let title: String
     let completed: Bool
 }
 
-let anyUrl = URLRequest(url: URL(string: "https://jsonplaceholder.typicode.com/todos/1")!)
+//  Fix URL
+let anyUrl = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
 
-func fetchData(theUrl url: URL, completion: @escaping (Result<Data,NetworkError>) -> Void) {
+func fetchData(theUrl url: URL, completion: @escaping (Result<DataToDecode,NetworkError>) -> Void) {
     URLSession.shared.dataTask(with: url) { data, response, error in
         guard let data else {
             completion(.failure(.badURL))
             return
         }
         do {
-            let data = try JSONDecoder().decode(dataToDecode.self, from: data)
+            let theResult = try JSONDecoder().decode(DataToDecode.self, from: data)
+            completion(.success(theResult))
         } catch {
             completion(.failure(.badURL))
         }
+    }.resume()
+}
+
+fetchData(theUrl: anyUrl) { result in
+    switch result {
+    case .success(let data):
+        print(data)
+    case .failure(let error):
+        print(error)
     }
 }
